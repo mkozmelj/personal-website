854757b66ed6726f44be09185b50d68e681cc95e
Martin Kozmelj <kozmelj.martin@gmail.com>
2024-02-25 19:36:37 +0100

    mobile navigation

--- src/components/common/title/index.tsx
+++ src/components/common/title/index.tsx
@@ -3,7 +3,7 @@ interface IProps {
 }
 export function Title({ title }: IProps) {
   return (
-    <h2 className="sticky top-0 lg:hidden font-bold mb-2 bg-background/70 px-8 py-2 lg-p-0">
+    <h2 className="sticky top-0 lg:hidden font-bold mb-2 bg-black-200/70 px-8 py-2 lg-p-0">
       {title}
     </h2>
   );
